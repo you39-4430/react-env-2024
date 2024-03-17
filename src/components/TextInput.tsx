47ae5d8ceeb3e0d20e7a6ffb0c87a142ceb20fb0
yusaku yoshizawa <you39.4430@gmail.com>
2024-03-17 18:05:57 +0900

    mod

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const TextInput = () => {
+    const [text, setText] = useState<string>("");
+
+    return (
+        <div>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} aria-label="Text Input" />
+            <p>Entered Text: {text}</p>
+        </div>
+    )
+}
+
+export default TextInput
